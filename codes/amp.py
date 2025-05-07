59634624eff1e7bda7a1587b3ba43d8e26c496a5
Asif Ashraf <aashraf@uoregon.edu>
2025-05-07 08:40:37 -0700

    update on filter and maxAmp picking

--- codes/amp.py
+++ codes/amp.py
@@ -1,13 +1,26 @@
-# Inputs
-station    = '30001'
-shots      = list(range(67178,70040))
+## Inputs
+station      = '30001'
+shots        = list(range(67178,67199))
 
-theShot    = '/Users/asifashraf/Documents/SEGY_conversion/GIPP_tools/Project_files/PD16.txt'
-theArrival = '/Users/asifashraf/Documents/MATLAB/Cascadia Analysis/Stingray_structures/tlArrival_chk6M35_06-Nov-2024_PPgPn_srModel_06-Nov-2024_CG_SG_softB_5.mat'
+shot_file    = 'PD16.txt'
+arrival_file = 'tlArrival_chk6M35_06-Nov-2024_PPgPn_srModel_06-Nov-2024_CG_SG_softB_5.mat'
 
-## (seconds) This time window determines the range of data after user selects the arrival
-timeWindow = .02      
+# shotfile
+import os
+script_dir  = os.path.dirname(os.path.abspath(__file__))
+parent_dir  = os.path.dirname(script_dir)
+inp_dir     = os.path.join(parent_dir, 'input')
+out_dir     = os.path.join(parent_dir, 'output')
 
+theShot     = inp_dir + '/' + shot_file
+theArrival  = inp_dir + '/' + arrival_file
+thePlot     = out_dir + '/plots/' + station
+theData     = out_dir + '/data/' + station
+
+# make those folders, if not in there
+os.makedirs(thePlot, exist_ok=True)
+os.makedirs(theData, exist_ok=True)
+  
 # Import modules
 import numpy as np
 from obspy.core import read
@@ -20,58 +33,116 @@ import scipy.io as sio
 import pandas as pd
 from datetime import datetime, timedelta
 
-for i in range(len(shots)):
-    try:
-        shot = str(shots[i])
-        print('shot: '+ shot)
-        # load the mseed file for that station
-        #st_path = ('/Volumes/LaCie/Ally_Project/' + station + '/' + station + '210620064043.mseed')
-        st_path = ('/Volumes/Cascadia_Exp_Backup2/Ally_project/' + station + '/' + station + '210620064043.dp3')
-        mseed   = read(st_path)
-        tr      = mseed[0]
+# make the output ascii file
+out_txt = os.path.join(theData, "shot_rms.txt")
+with open(out_txt, "w", encoding="utf-8") as f:
 
-        # Load the tlArrival structure
-        data      = sio.loadmat(theArrival)
-        tlArrival = data['tlArrival']
-        #Access the structure's fields; use:tlArrival.dtype.names to show the fields
-        events   = tlArrival['eventid']
-        stations = tlArrival['station']
-        times    = tlArrival['time']
-        # make arrays for events, stations and times
-        events   = events[0][0]
-        stations = stations[0][0]
-        times    = times[0][0]
-        idx_shot    = np.where(events == int(shot))[0]
-        idx_station = np.where(stations == (station))[0]
-        cmmn        = np.intersect1d(idx_station, idx_shot)
-        if len(cmmn) > 0:
-                print('Arrival pick for the station-shot exists!')
-                print('The station-shot index:' + str(cmmn))
-        else:
-                print("No station-shot combination found")
+    f.write("shot\tRMS-amp\n")
 
-        traveltime = times[cmmn]; traveltime = traveltime[0][0]
-        print('Picked time: ' + str(traveltime) + ' seconds')
+    for i in range(len(shots)):
+        try:
+            shot = str(shots[i])
+            print('shot: '+ shot)
+            # load the mseed file for that station
+            #st_path = ('/Volumes/LaCie/Ally_Project/' + station + '/' + station + '210620064043.mseed')
+            st_path = ('/Volumes/Cascadia_Exp_Backup2/Ally_project/' + station + '/' + station + '210620064043.dp3')
+            mseed   = read(st_path)
+            tr      = mseed[0]
 
-        # Load the shot data
-        df          = pd.read_csv(theShot, delim_whitespace=True)
-        shotno      = np.array(df.iloc[:,1])                       # All shots
-        shottimes   = df.iloc[:,6]                                 # All shot-times
-        idx_shotID  = np.where(shotno == int(shot))                # Find the shot index
-        shottime    = shottimes[idx_shotID[0][0]]                  # Shottime for the desired shot
-        shottime_dt = UTCDateTime(shottime)                        # Convert that shottime to a UTCdatetime format
+            # Load the tlArrival structure
+            data      = sio.loadmat(theArrival)
+            tlArrival = data['tlArrival']
+            #Access the structure's fields; use:tlArrival.dtype.names to show the fields
+            events   = tlArrival['eventid']
+            stations = tlArrival['station']
+            times    = tlArrival['time']
+            # make arrays for events, stations and times
+            events   = events[0][0]
+            stations = stations[0][0]
+            times    = times[0][0]
+            idx_shot    = np.where(events == int(shot))[0]
+            idx_station = np.where(stations == (station))[0]
+            cmmn        = np.intersect1d(idx_station, idx_shot)
+            if len(cmmn) > 0:
+                    print('     Arrival pick for the station-shot exists!')
+                    print('     The station-shot index:' + str(cmmn))
+            else:
+                    print("     No station-shot combination found")
 
-        arrivalTime = UTCDateTime(shottime_dt + traveltime)                      # Calculate the arrival time for that shot
-        before_shot = arrivalTime - 20                              # sec before the shot arrival
-        after_shot  = arrivalTime + 20                              # sec after the shot arrival
+            traveltime = times[cmmn]; traveltime = traveltime[0][0]
+            print('         Picked time: ' + str(traveltime) + ' seconds')
 
-        tr_slice = tr.slice(starttime=before_shot, endtime=after_shot)
+            # Load the shot data
+            df          = pd.read_csv(theShot, delim_whitespace=True)
+            shotno      = np.array(df.iloc[:,1])                       # All shots
+            shottimes   = df.iloc[:,6]                                 # All shot-times
+            idx_shotID  = np.where(shotno == int(shot))                # Find the shot index
+            shottime    = shottimes[idx_shotID[0][0]]                  # Shottime for the desired shot
+            shottime_dt = UTCDateTime(shottime)                        # Convert that shottime to a UTCdatetime format
 
-        tmAx = np.linspace(1, 20*2, len(tr_slice.data))
+            arrivalTime = UTCDateTime(shottime_dt + traveltime)                      # Calculate the arrival time for that shot
+            before_shot = arrivalTime - 20                              # sec before the shot arrival
+            after_shot  = arrivalTime + 20                              # sec after the shot arrival
 
-        plt.close('all')
-        plt.figure(1)
-        plt.plot(tmAx, tr_slice.data)
-        plt.savefig("/Users/asifashraf/Documents/code_repo/ampSeismic/plots/shot_" + shot +"_data.png", dpi=300, bbox_inches='tight')
-    except:
-        pass
+            tr_slice  = tr.slice(starttime=before_shot, endtime=after_shot)
+            tr_slice2 = tr.slice(starttime=arrivalTime, endtime=arrivalTime + 1)
+
+            # Apply a Butterworth bandpass filter to the trace
+            tr_slice.filter("bandpass", freqmin=3.0, freqmax=12.0, corners=4, zerophase=True)
+            tr_slice2.filter("bandpass", freqmin=3.0, freqmax=12.0, corners=4, zerophase=True)
+
+            tmAx  = np.linspace(-20, 20, len(tr_slice.data))
+            tmAx2 = np.linspace(0, 1, len(tr_slice2.data))
+
+            # find where the maximum amplitude occurs
+            idx_maxAmp = np.where(tr_slice2.data == np.max(tr_slice2.data))
+            idx_maxAmp = idx_maxAmp[0][0]
+            tm_maxAmp  = tmAx2[idx_maxAmp]
+            tr_slice3  = tr.slice(starttime=(arrivalTime+tm_maxAmp - .15), endtime=(arrivalTime+tm_maxAmp + .15))
+
+            rms_amp    = np.sqrt(np.mean(abs(tr_slice3.data)))
+
+            f.write(f"{int(shot)}\t{rms_amp}\n")
+
+        
+            plt.close('all')
+            plt.figure(figsize=(10, 10))
+
+            # First subplot: with y-axis limits
+            plt.subplot(3, 1, 1)   # (nrows, ncols, index)
+            plt.plot(tmAx, tr_slice.data)
+            plt.title(f'Filtered Y-axis Data (Shot {shot})')
+            plt.xlabel('Time relative to arrival [s]')
+            plt.ylabel('Amplitude')
+            plt.ylim([-0.015, 0.015])
+            plt.axvline(0, color='r', linestyle='--', linewidth=1, label='Arrival Time')
+            plt.legend()
+            plt.grid()
+
+            # Second subplot: without y-axis limits
+            plt.subplot(3, 1, 2)
+            plt.plot(tmAx, tr_slice.data)
+            plt.title(f'Filtered Y-axis Data (Shot {shot}) - Zoomed In')
+            plt.xlabel('Time relative to arrival [s]')
+            plt.xlim(-1,3)
+            plt.ylabel('Amplitude')
+            plt.axvline(0, color='r', linestyle='--', linewidth=1, label='Arrival Time')
+            plt.legend()
+            plt.grid()
+
+            plt.subplot(3, 1, 3)   # (nrows, ncols, index)
+            plt.plot(tmAx2, tr_slice2.data)
+            plt.title(f'Filtered Y-axis Data (Shot {shot}) - Further zoomed in')
+            plt.xlabel('Time relative to arrival [s]')
+            plt.ylabel('Amplitude')
+            plt.axvline(0, color='r', linestyle='--', linewidth=1, label='Arrival Time')
+            plt.axvline(tmAx2[idx_maxAmp], color='g', linestyle='--', linewidth=1, label='Max Amplitude')
+            plt.plot([0+tm_maxAmp - .15, 0+tm_maxAmp + .15], [0, 0], '-g')
+            plt.legend()
+            plt.grid()
+
+            plt.tight_layout()
+            plt.savefig(thePlot + '/shot' + shot + ".png", dpi=300, bbox_inches='tight')
+
+        except:
+            pass
